a3c2cdcdbe74ba7f3ebd7093d38212c16ffa3bd5
eguantay <eguantay@silentiumapps.com>
2020-02-14 11:18:58 -0300

    Modificando formulario de perfil

--- components/account/changeDisplayNameForm.tsx
+++ components/account/changeDisplayNameForm.tsx
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View,  Text } from 'react-native';
 import {Input, Button} from 'react-native-elements';
+import * as fireBase from 'firebase';
 
 const ChangeDisplayNameForm = (prop:any)=>{
+
     //propr
     const {displayName, setIsVisibleModal, setReloadData, toastRef} = prop;
-    console.log(prop);
+    const[newDisplayName, setNewDisplayName] = useState();
+    const[error,setError] = useState();
+    const[isLoading, setIsLoading] = useState(false);
 
     const updateDisplayName = ()=>{
         console.log('Nombre de usuario actualizado');
+        setError(null);
+        if(!newDisplayName){
+            setError('El nombre de usuario no ha cambiado');
+        }else{
+            setIsLoading(true);
+            const update ={
+                displayName: newDisplayName
+            }
+            fireBase
+            .auth()
+            .currentUser?.updateProfile(update).then(()=>{
+                setIsLoading(false);
+                setReloadData(true);
+                toastRef.current.show('Nombre actualizado correctamente');
+                setIsVisibleModal(false);
+            })
+            .catch(()=>{
+                setError('Error al actualizar el nombre');
+                setIsLoading(false);
+            })
+        }
     }
 
     return(
         <View style={styles.view}>
             
-
             <Input
                 placeholder ="Nombre"
                 containerStyle = {styles.input}
                 defaultValue ={displayName && displayName}
-                //onChange = {()=>{}}
+                onChange = {e=> setNewDisplayName(e.nativeEvent.text)}
+                //onChangeText = { e => setNewDisplayName(e)}
+                
                 rightIcon={{
                     type: "material-community",
                     name: "account-circle-outline",
                     color: "#c2c2c2"
                 }}
-                //errorMessage ={}
+                errorMessage ={error}
             />
 
             <Button 
@@ -33,10 +59,8 @@ const ChangeDisplayNameForm = (prop:any)=>{
                 containerStyle = {styles.btnContainer}
                 buttonStyle = {styles.btn}
                 onPress = {updateDisplayName}
-                //loading = {}
+                loading = {isLoading}
             />
-
-            
         </View>
     );
 }
@@ -51,6 +75,7 @@ const styles = StyleSheet.create({
     },
     btnContainer : {
         marginTop: 20,
+        width: '100%',
     },
     btn : {
         backgroundColor : "#00a680",
